cb4ea38684631128df7453802906ec2cdc98a9e0
MythicalPlayz <eyaddodo05@gmail.com>
2024-07-28 23:58:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Layout from './components/Layout/Layout'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom'
 import About from './components/About/About'
 import Home from './components/Home/Home'
 import E404 from './components/E404/E404'
 import Portfolio from './components/Portfolio/Portfolio'
 import Contact from './components/Contact/Contact'
 
-const router = createBrowserRouter([
+const router = createHashRouter([  //createBrowserRouter Broken on Github
   {
     path: '', element: <Layout/>, children: [
       {index: true, element: <Home/>},
